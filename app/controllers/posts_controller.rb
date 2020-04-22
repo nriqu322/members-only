995d44f1bf1b94263399aaf0e3c32effe29c5ebe
Luis Saavedra <nriqu322@gmail.com>
2020-04-22 14:43:51 -0600

    removed methods edit and show from post crontoller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,18 +9,10 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
-  # GET /posts/1
-  # GET /posts/1.json
-  def show; end
-
   # GET /posts/new
   def new
     @post = current_user.posts.build
-    # @post = Post.new
   end
-
-  # GET /posts/1/edit
-  def edit; end
 
   # POST /posts
   # POST /posts.json
